e2cd34c02300b709f308cb9173fd143c0fe5e98c
Andrew McOlash <amcolash@gmail.com>
2019-03-24 15:56:29 -0700

    Sign in / out user auth

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component, Fragment } from 'react';
 import moment from 'moment';
 import { debounce } from 'debounce';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDoubleLeft, faAngleDoubleRight, faCheck, faExpandArrowsAlt, faCompressArrowsAlt, faLongArrowAltDown, faMoon, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDoubleLeft, faAngleDoubleRight, faCheck, faExpandArrowsAlt, faCompressArrowsAlt, faLongArrowAltDown,
+  faMoon, faPlus, faTrash, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 import firebase from './firebase.js';
 import './App.css';
 
@@ -19,7 +21,9 @@ class App extends Component {
       initialLoad: true,
       darkMode: true,
       fullscreen: false,
-      sidebar: true
+      sidebar: true,
+      initialAuth: true,
+      user: undefined
     };
 
     this.handleTitleChange = this.handleTitleChange.bind(this);
@@ -28,9 +32,17 @@ class App extends Component {
   }
   
   componentDidMount() {
+    firebase.app.auth().onAuthStateChanged(u => {
+      if (u) {
+        this.setState({user: u, initialAuth: false});
+      } else {
+        this.setState({user: undefined, initialAuth: false});
+      }
+    });
+
     // Updating the `itemList` local state attribute when the Firebase Realtime Database data
     // under the '/itemList' path changes.
-    this.firebaseRef = firebase.database().ref('/itemList');
+    this.firebaseRef = firebase.app.database().ref('/itemList');
     this.firebaseCallback = this.firebaseRef.on('value', (snap) => {
       const values = snap.val();
 
@@ -123,82 +135,99 @@ class App extends Component {
   }
 
   render() {
-    const { initialLoad, saved, selected, itemList, itemTitle, itemText, darkMode, fullscreen, sidebar } = this.state;
+    const { initialLoad, saved, selected, itemList, itemTitle, itemText, darkMode, fullscreen, sidebar, user, initialAuth } = this.state;
 
     return (
-      <div className={"App" + (darkMode ? "" : " inverted")}>
-        {initialLoad ? null : (
-          <Fragment>
-            {itemList.length === 0 ? (
-              <div className="noNotes">
-                <h2>
-                  You don't have any notes yet.
-                  <br/>
-                  Click the plus button to get started.
-                </h2>
-                <FontAwesomeIcon icon={faLongArrowAltDown} className="arrow" />
-              </div>
-            ) : (
-              <Fragment>
-                <div className={"column left" + (sidebar ? "" : " collapsed")}>
-                  <div className="dates">
-                    {itemList ? Object.entries(itemList).map(([index, item]) => {
-                      index = Number.parseInt(index);
-                      return (
-                        <div className="item" key={index}>
-                          <button className={"button date" + (selected === index ? " selected" : "")} onClick={() => this.select(index)}>
-                            {selected === index ? itemTitle : item.value.title}
-                            <br/>
-                            <span className="updated">{moment(item.value.updated).fromNow()}</span>
-                          </button>
-                          <button className={"button delete" + (darkMode ? "" : " inverted")} onClick={() => this.delete(item.id)}>
-                            <FontAwesomeIcon icon={faTrash} />
-                          </button>
-                        </div>
-                      )
-                    }) : null}
-                  </div>
-                  <div className={"footer" + (!sidebar ? " collapsed" : "")}>
-                    <div className="spacer"></div>
-                    <button className={"button" + (darkMode ? " selected" : "")} onClick={() => this.setState({darkMode: !darkMode})}>
-                      <FontAwesomeIcon icon={faMoon}/>
-                    </button>
-
-                    <button className={"button" + (fullscreen ? " selected" : "")} onClick={() => {
-                      if (fullscreen) {
-                        document.exitFullscreen();
-                      } else {
-                        document.documentElement.requestFullscreen();
-                      }
-
-                      this.setState({fullscreen: !fullscreen});
-                    }}>
-                      <FontAwesomeIcon icon={fullscreen ? faCompressArrowsAlt : faExpandArrowsAlt}/>
-                    </button>
-
-                    <div className="spacer"></div>
-                    <button className={"button"} onClick={() => this.setState({sidebar: !sidebar})}>
-                      <FontAwesomeIcon icon={sidebar ? faAngleDoubleLeft : faAngleDoubleRight}/>
-                    </button>
-                  </div>
-                </div>
-        
-                <div className={"column right" + (sidebar ? "" : " expanded")}>
-                  <div>
-                    <h3>
-                      <input className="title" type="text" value={itemTitle} onChange={this.handleTitleChange}/>
-                      <FontAwesomeIcon icon={faCheck} className={"saved" + (saved ? "" : " hidden") + (darkMode ? "" : " specialInverted")} />
-                    </h3>
-                  </div>
-                  <textarea value={itemText} onChange={this.handleTextChange}/>
-                </div>
-              </Fragment>
-            )}
-  
-            <button className={"circle" + (itemList.length === 0 ? " glow" : "")} onClick={this.handleCreate}>
-              <FontAwesomeIcon icon={faPlus}/>
+      <div className={"App" + (darkMode ? "" : " inverted") + (!user && !initialAuth ? " center" : "")}>
+        {!user && !initialAuth ? (
+          <div className="signin">
+            <span>You are not authenticated.</span><br/>
+            <span>You need to sign in to use this app.</span><br/>
+            <br/>
+            <button title="Sign In" className="button" onClick={() => firebase.signIn()}>
+              <FontAwesomeIcon icon={faGoogle} className="padRight" />
+              Sign In
             </button>
-          </Fragment>
+          </div>
+        ) : (
+          initialLoad ? null : (
+            <Fragment>
+              {itemList.length === 0 ? (
+                <div className="noNotes">
+                  <h2>
+                    You don't have any notes yet.
+                    <br/>
+                    Click the plus button to get started.
+                  </h2>
+                  <FontAwesomeIcon icon={faLongArrowAltDown} className="arrow" />
+                </div>
+              ) : (
+                <Fragment>
+                  <div className={"column left" + (sidebar ? "" : " collapsed")}>
+                    <div className="dates">
+                      {itemList ? Object.entries(itemList).map(([index, item]) => {
+                        index = Number.parseInt(index);
+                        return (
+                          <div className="item" key={index}>
+                            <button className={"button date" + (selected === index ? " selected" : "")} onClick={() => this.select(index)}>
+                              {selected === index ? itemTitle : item.value.title}
+                              <br/>
+                              <span className="updated">{moment(item.value.updated).fromNow()}</span>
+                            </button>
+                            <button title="Delete" className={"button delete" + (darkMode ? "" : " inverted")} onClick={() => this.delete(item.id)}>
+                              <FontAwesomeIcon icon={faTrash} />
+                            </button>
+                          </div>
+                        )
+                      }) : null}
+                    </div>
+                    <div className={"footer" + (!sidebar ? " collapsed" : "")}>
+                      <button title="Sign Out" className="button" onClick={() => firebase.app.auth().signOut()}>
+                        <FontAwesomeIcon icon={faSignOutAlt}/>
+                      </button>
+
+                      <div className="spacer"></div>
+
+                      <button title="Toggle Dark Mode" className={"button" + (darkMode ? " selected" : "")} onClick={() => this.setState({darkMode: !darkMode})}>
+                        <FontAwesomeIcon icon={faMoon}/>
+                      </button>
+
+                      <button title="Toggle Full Screen" className={"button" + (fullscreen ? " selected" : "")} onClick={() => {
+                        if (fullscreen) {
+                          document.exitFullscreen();
+                        } else {
+                          document.documentElement.requestFullscreen();
+                        }
+
+                        this.setState({fullscreen: !fullscreen});
+                      }}>
+                        <FontAwesomeIcon icon={fullscreen ? faCompressArrowsAlt : faExpandArrowsAlt}/>
+                      </button>
+
+                      <div className="spacer"></div>
+                      <button title="Toggle Sidebar" className={"button"} onClick={() => this.setState({sidebar: !sidebar})}>
+                        <FontAwesomeIcon icon={sidebar ? faAngleDoubleLeft : faAngleDoubleRight}/>
+                      </button>
+                    </div>
+                  </div>
+          
+                  <div className={"column right" + (sidebar ? "" : " expanded")}>
+                    <div>
+                      <h3>
+                        <input className="title" type="text" value={itemTitle} onChange={this.handleTitleChange}/>
+                        <FontAwesomeIcon icon={faCheck} className={"saved" + (saved ? "" : " hidden") + (darkMode ? "" : " specialInverted")} />
+                      </h3>
+                    </div>
+                    <textarea value={itemText} onChange={this.handleTextChange}/>
+                  </div>
+                </Fragment>
+              )}
+    
+              <button title="New Item" className={"circle" + (itemList.length === 0 ? " glow" : "")} onClick={this.handleCreate}>
+                <FontAwesomeIcon icon={faPlus}/>
+              </button>
+            </Fragment>
+          )
         )}
       </div>
     );
--- src/firebase.js
+++ src/firebase.js
@@ -13,4 +13,32 @@ const config = {
   messagingSenderId: "335873153280"
 };
 
-export default firebase.initializeApp(config);
+const app = firebase.initializeApp(config);
+
+function signIn() {
+  app.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(() => {
+    app.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(result => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(token, user);
+      // ...
+    }).catch(error => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      console.log(errorCode, errorMessage, email, credential);
+      // ...
+    });
+  });
+}
+
+export default {
+  app,
+  signIn
+};
